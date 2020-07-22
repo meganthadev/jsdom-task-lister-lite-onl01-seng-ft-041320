ab1da5d1c91c28d03eac5d8ce0cecd8c37f4d58c
Megan Horsfield <meganthadev@gmail.com>
2020-07-22 05:10:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+let tasks = document.getElementById('tasks');
+let newToDo = document.getElementById('new-task-description');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let toDo = newToDo.value;
+  let toDoItem = document.createElement('li');
+   let toDoItemText = document.createTextNode(toDo);
+   toDoItem.appendChild(toDoItemText);
+   tasks.appendChild(toDoItem);
+    newToDo.value = "";
 });
